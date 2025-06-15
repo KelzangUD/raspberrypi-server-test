fd77b1f4438efbdc32525a6751fa989e10dd58d3
KelzangUD <kuding755@gmail.com>
2025-06-15 11:42:14 +0600

    index U and package update

--- index.js
+++ index.js
@@ -198,6 +198,28 @@ app.post("/gpio", async (req, res) => {
   }
 });
 
+// Command to ping a slave
+app.post("/ping", (req, res) => {
+  const { ip } = req.body;
+  exec(`ping -c 1 ${ip}`, (error, stdout, stderr) => {
+    if (error) {
+      return res.status(500).send(`Ping failed: ${stderr}`);
+    }
+    res.send(`Ping successful:\n${stdout}`);
+  });
+});
+
+// Command to SSH and run a command on a slave
+app.post("/command", (req, res) => {
+  const { ip, cmd } = req.body;
+  exec(`ssh pi@${ip} "${cmd}"`, (error, stdout, stderr) => {
+    if (error) {
+      return res.status(500).send(`Command failed: ${stderr}`);
+    }
+    res.send(`Command output:\n${stdout}`);
+  });
+});
+
 // Start Server
 app.listen(PORT, () => {
   console.log(
